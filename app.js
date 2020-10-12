96d279fe7f61fcaa8544be99db5303ecc4b695f7
AJ Torres <torresariel017@gmail.com>
2020-10-12 12:13:16 +0800

    Changes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const urlEncodedParser = bodyParser.urlencoded({extended:false});
-const connection = require("mysql");
+var session = require("express-session");
+const mysql = require("mysql");
+const { response } = require("express");
 const app = express();
 
 app.use(express.static('public'));
@@ -12,22 +16,103 @@ app.use('/img', express.static(__dirname + 'public/img'));
 app.use(express.static('node_modules'));
 app.use('/@fontawesome', express.static(__dirname + 'node_modules/fontawesome'));
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
 app.set('view engine', 'ejs');
 
+const connection = mysql.createConnection({
+    host: "127.0.0.11",
+    user: "root",
+    password:"",
+    database: "inventory"
+})
+
+connection.connect((err)=>{
+    if(err) throw err;
+    console.log("Database connected");
+})
+
+
+app.post("/create_user", (req, res)=>{
+    console.log(req.query);
+    let salt = bcrypt.genSaltSync(saltRounds);
+    let hash = bcrypt.hashSync(req.query.password, salt);
+    connection.query("INSERT INTO user (userName, password, userType) VALUES ('"+req.query.userName+"', '"+hash+"', '"+req.query.userType+"')", (err, response)=>{
+        if(err) throw err;
+        res.send("Here");
+    })
+})
+
+//Login
+app.get('/login', (req, res)=>{
+    res.render('login');
+})
+
+app.post('/auth', urlEncodedParser, (req, res)=>{
+    connection.query('SELECT * FROM user WHERE userName = "'+req.body.username+'"', (err, response)=>{
+        if(err) throw err;
+        if(response.length > 0){
+            if(bcrypt.compareSync(req.body.password, response[0]['password'])){
+                req.session.loggedin = true;
+                req.session.username = req.body.username;
+                connection.query('SELECT userID FROM user WHERE userName = "'+req.body.username+'" AND password = "'+req.body.password+'"', (err, response,result)=>{
+                   req.session.userID = result.userID;
+                })
+                res.redirect('/');
+            }else{
+                res.send('Incorrect Username or Password!');
+            }
+        }else{
+            res.end();
+        }
+    });
+})
+
+
+//Dashboard
 app.get('/', (req, res)=>{
-    res.render('dash');
+    if(req.session.loggedin){
+        res.render('dash');
+    }
+})
+
+//Product
+app.get('/showprod', (req, res)=>{
+    connection.query('SELECT productId, productName,unitPrice, dateAdded, variantDesc, qty FROM product INNER JOIN product_variant ON product.productId = product_variant.product_Id AND product.status = 1', (err, result)=>{
+        res.render('showprod', {prods: result});
+    })
 })
 
 app.get('/addprod', (req, res)=>{
-    res.render('addprod');
+    connection.query('SELECT * FROM product WHERE status = 1', (err, result)=>{
+        res.render('addprod', {prod: result});
+    });  
 })
 
-app.get('/showprod', (req, res)=>{
-    res.render('showprod');
+app.post('/addprod', urlEncodedParser, (req, res)=>{
+    connection.query('INSERT INTO product(productName, unitPrice, userId) VALUES ("'+req.body.prodName+'", '+req.body.price+', '+req.session.userID+')', (err)=>{
+        if(err) throw err;
+        res.redirect('/todo');
+    });
+    console.log(req.session.userID);
+})
+
+
+app.get('/delete', (req, res)=>{
+    connection.query("UPDATE product SET status = 0 WHERE productId = ?", req.query.id, (err)=>{
+        if(err) throw err;
+        res.redirect('/addprod');
+    });
 })
 
 app.get('/transaction', (req, res)=>{
     res.render('transaction');
 })
+
+
 
 app.listen(3000);
